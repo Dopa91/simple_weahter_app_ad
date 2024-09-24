9d1999318c73335892cd0b85f9fb486b11036c19
Dopa91 <code.dopatka@gmail.com>
2024-09-24 22:04:38 +0200

    Bonus: Mehr Wetter

--- lib/weather_repo.dart
+++ lib/weather_repo.dart
@@ -1,10 +1,32 @@
 import 'weather_data.dart';
+import 'dart:math';
 
 class WeatherRepo {
-  final WeatherData _weatherData = WeatherData(
-      city: "Krefeld", temperature: 24.4, weaterCondition: "Cloudy");
+  final List<WeatherData> _weatherData = [
+    WeatherData(city: "Krefeld", temperature: 24.4, weatherCondition: "Sunny"),
+    WeatherData(city: "Berlin", temperature: 18.8, weatherCondition: "Cloudy"),
+    WeatherData(city: "Stettin", temperature: 21, weatherCondition: "Sunny"),
+    WeatherData(city: "München", temperature: 22.2, weatherCondition: "Stormy"),
+    WeatherData(city: "Hamburg", temperature: 19.8, weatherCondition: "Rainy"),
+    WeatherData(
+        city: "Düsseldorf", temperature: 23.8, weatherCondition: "Sunny"),
+  ];
 
   WeatherData getWeather() {
-    return _weatherData;
+    final random = Random();
+    return _weatherData[random.nextInt(_weatherData.length)];
   }
 }
+
+
+
+
+
+// class WeatherRepo {
+//   final WeatherData _weatherData =
+//       WeatherData(city: "Krefeld", temperature: 24.4, weaterCondition: "Sunny");
+
+//   WeatherData getWeather() {
+//     return _weatherData;
+//   }
+// }
